631ee168b98a868d9cfa31205c1b1098e7cc0010
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-07 02:44:04 -0500

    Creates class SudokuException to be used by the SudokuBoard class

---
+++ src/SudokuException.java
@@ -0,0 +1,11 @@
+/**
+ * Homework #6 Created by Evan Almonte on 12/7/2015.
+ */
+public class SudokuException extends Exception {
+    SudokuException() {
+        super();
+    }
+    SudokuException(String errorMessage) {
+        super(errorMessage);
+    }
+}
