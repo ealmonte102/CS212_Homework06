3a5f128d379ffcd724cae7f4d67c9272f6b07931
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-07 00:23:56 -0500

    Creates class SudokuSquareNode

---
+++ src/SudokuSquareNode.java
@@ -0,0 +1,22 @@
+/**
+ * Homework #6 Created by Evan Almonte on 12/7/2015.
+ */
+public class SudokuSquareNode {
+    private SudokuSquare data;
+    private SudokuSquareNode next;
+
+    public SudokuSquareNode(SudokuSquare data) {
+        this.data = data;
+    }
+
+    public SudokuSquareNode(SudokuSquare data, SudokuSquareNode next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public SudokuSquare getData() { return data; }
+
+    public SudokuSquareNode getNext() { return next; }
+
+    public void setNext(SudokuSquareNode next) { this.next = next; }
+}
