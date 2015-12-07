47b57e5f85ab65eef1a183557b847834a79cd042
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-07 00:51:08 -0500

    Creates class SudokuSquareLinkedList
    -	Represents a LinkedList that holds SudokuSquareNodes

---
+++ src/SudokuSquareLinkedList.java
@@ -0,0 +1,26 @@
+/**
+ * Homework #6 Created by Evan Almonte on 12/7/2015.
+ */
+public class SudokuSquareLinkedList {
+    private SudokuSquareNode head;
+    SudokuSquareNode squareToReturn;
+
+    public SudokuSquareLinkedList(SudokuSquareNode head) {
+        this.head = head;
+        squareToReturn = head;
+    }
+
+    public void append(SudokuSquareNode end) {
+        SudokuSquareNode current = head;
+        while(current.getNext() != null) {
+            current = current.getNext();
+        }
+        current.setNext(end);
+    }
+
+    public SudokuSquareNode getNext() {
+        SudokuSquareNode aSudokuNode = squareToReturn;
+        squareToReturn = squareToReturn.getNext();
+        return aSudokuNode;
+    }
+}
