7dda81c769f30e9d27c6c18945ec5ef373a41ee2
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-11 01:20:33 -0500

    Partially implements the default constructor for SudokuBoardView

--- src/SudokuBoardView.java
+++ src/SudokuBoardView.java
@@ -2,6 +2,7 @@
  * Created by Evan Almonte on 12/11/2015.
  */
 import javax.swing.*;
+import java.awt.*;
 
 public class SudokuBoardView extends JFrame {
     private SudokuBoard theBoard;
@@ -11,4 +12,42 @@ public class SudokuBoardView extends JFrame {
     private JRadioButton clearRadio;
     private JTextField inputField;
     private JButton [][] sudokuButtons;
+
+    public SudokuBoardView(SudokuBoard theBoard, int height, int width) {
+        super("Welcome To Sudoku");
+        setSize(height, width);
+        initComponents();
+    }
+
+    private void initComponents() {
+        initOutputField();
+        initRadioButtons();
+        JPanel topContent = new JPanel(new BorderLayout());
+        JPanel topContentOpts = new JPanel(new GridBagLayout());
+        initTopPanel(topContent, topContentOpts);
+        JPanel boardPanel = new JPanel(new GridLayout(theBoard.boardSize, theBoard.boardSize));
+        initSudokuBtns(boardPanel);
+        initInputField();
+        add(boardPanel, BorderLayout.CENTER);
+        add(topContent, BorderLayout.NORTH);
+        add(inputField, BorderLayout.SOUTH);
+    }
+
+    private void initOutputField() {
+        //Intentionally Left Empty.
+    }
+    private void initInputField() {
+        //Intentionally Left Empty.
+    }
+    private void initRadioButtons() {
+        //Intentionally Left Empty.
+    }
+    private void initTopPanel(JPanel panel, JPanel optionPanel) {
+        //Intentionally Left Empty.
+    }
+    private void initSudokuBtns(JPanel boardPanel) {
+        //Intentionally Left Empty.
+    }
+
+
 }
