c11786c4a975553f5073ac1e7c1189a881d7fec3
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-11 02:34:07 -0500

    Adds a default constructor which sets null values
    -	Edits the function append to properly check if there is a head node.

--- src/SudokuSquareLinkedList.java
+++ src/SudokuSquareLinkedList.java
@@ -5,14 +5,24 @@ public class SudokuSquareLinkedList {
     private SudokuSquareNode head;
     SudokuSquareNode squareToReturn;
 
+    public SudokuSquareLinkedList() {
+        this.head = null;
+        squareToReturn = null;
+    }
+
     public SudokuSquareLinkedList(SudokuSquareNode head) {
         this.head = head;
         squareToReturn = head;
     }
 
     public void append(SudokuSquareNode end) {
+        if(head == null) {
+            head = end;
+            squareToReturn = head;
+            return;
+        }
         SudokuSquareNode current = head;
-        while(current.getNext() != null) {
+        while (current.getNext() != null) {
             current = current.getNext();
         }
         current.setNext(end);
