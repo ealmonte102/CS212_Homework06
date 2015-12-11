21901d8282b36a2b59d5aefd71666133a2a29b18
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-11 01:11:37 -0500

    Creates class SudokuBoardView

---
+++ src/SudokuBoardView.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Evan Almonte on 12/11/2015.
+ */
+import javax.swing.*;
+
+public class SudokuBoardView extends JFrame {
+    private SudokuBoard theBoard;
+    private JTextField outputField;
+    private JRadioButton checkIfValidRadio;
+    private JRadioButton enterRadio;
+    private JRadioButton clearRadio;
+    private JTextField inputField;
+    private JButton [][] sudokuButtons;
+}
