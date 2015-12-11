d67b0ac837937979e12ea930fe5bac4118c7c100
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-11 02:44:43 -0500

    Adds class Homework06 which serves as the main method
    -	Adds the text file used to import the starting board.

--- src/Homework06.java
+++ src/Homework06.java
@@ -1,30 +1,37 @@
 /**
  * Homework #6 Created by Evan Almonte on 12/7/2015.
  */
+import java.io.*;
 import javax.swing.JOptionPane;
-import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Homework06 {
     public static void main(String[] args) {
-        SudokuSquareLinkedList startingBoard =
-                new SudokuSquareLinkedList(new SudokuSquareNode(new SudokuSquare(0,1,1,true)));
-        startingBoard.append(new SudokuSquareNode(new SudokuSquare(0,2,3,true)));
-        startingBoard.append(new SudokuSquareNode(new SudokuSquare(1,0,2,true)));
-        startingBoard.append(new SudokuSquareNode(new SudokuSquare(2,3,3,true)));
-        startingBoard.append(new SudokuSquareNode(new SudokuSquare(3,1,2,true)));
-        startingBoard.append(new SudokuSquareNode(new SudokuSquare(3,2,1,true)));
-        test(new SudokuBoard(startingBoard));
+        SudokuBoard startingBoard;
+        try {
+            startingBoard = new SudokuBoard(getBoardFromFile());
+        } catch(IOException ioe) {
+            System.out.println("ERROR, file not found.");
+            return;
+        }
+        SudokuBoardView myView = new SudokuBoardView(startingBoard, 500, 500);
+        SudokuBoardController sudokuController = new SudokuBoardController(startingBoard, myView);
+
     }
 
     public static void test(SudokuBoard gameBoard) {
         printBoard(gameBoard);
         while(!gameBoard.isFull()) {
-            String [] response = JOptionPane.showInputDialog(null, "Please enter row, column, and value").split(",");
-            int r = Integer.parseInt(response[0]);
-            int c = Integer.parseInt(response[1]);
-            int value = Integer.parseInt(response[2]);
             try {
+                String [] response = JOptionPane.showInputDialog(null, "Please enter row, column, and value").split(",");
+                int r = Integer.parseInt(response[0]);
+                int c = Integer.parseInt(response[1]);
+                int value = Integer.parseInt(response[2]);
                 gameBoard.enterMove(r, c, value);
+            } catch (NullPointerException npe) {
+                int response = JOptionPane.showConfirmDialog(null, "Would you like to quit?", "Choose an Option",
+                        JOptionPane.YES_NO_OPTION);
+                if(response == JOptionPane.YES_OPTION) { return; }
             } catch(SudokuException se) {
                 System.out.println(se.getMessage());
             }
@@ -41,5 +48,27 @@ public class Homework06 {
             System.out.println();
         }
         System.out.println();
+    }
+
+    public static SudokuSquareLinkedList getBoardFromFile() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("board.txt")));
+        String line = br.readLine();
+        SudokuSquareLinkedList boardFromFile = new SudokuSquareLinkedList();
+        while(line != null) {
+            StringTokenizer tokenLine = new StringTokenizer(line, ",");
+            try {
+                int row = Integer.parseInt(tokenLine.nextToken());
+                int column = Integer.parseInt(tokenLine.nextToken());
+                int value = Integer.parseInt(tokenLine.nextToken());
+                boardFromFile.append(new SudokuSquareNode(new SudokuSquare(row,column,value,true)));
+            } catch(NumberFormatException nfe) {
+                System.out.println("File has invalid input.");
+                return null;
+            }
+            line = br.readLine();
+        }
+
+
+        return boardFromFile;
     }
 }
